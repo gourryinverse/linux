8b944f40e5939f15047841562eb1c781e7a38358
Gregory Price <gourry@gourry.net>
2026-01-08 15:37:49 -0500

    mm: constify oom_control, scan_control, and alloc_context nodemask
    The nodemasks in these structures may come from a variety of sources,
    including tasks and cpusets - and should never be modified by any code
    when being passed around inside another context.
    
    Signed-off-by: Gregory Price <gourry@gourry.net>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3724,7 +3724,7 @@ try_this_zone:
 	return NULL;
 }
 
-static void warn_alloc_show_mem(gfp_t gfp_mask, nodemask_t *nodemask)
+static void warn_alloc_show_mem(gfp_t gfp_mask, const nodemask_t *nodemask)
 {
 	unsigned int filter = SHOW_MEM_FILTER_NODES;
 
@@ -3743,7 +3743,7 @@ static void warn_alloc_show_mem(gfp_t gfp_mask, nodemask_t *nodemask)
 	__show_mem(filter, nodemask, gfp_zone(gfp_mask));
 }
 
-void warn_alloc(gfp_t gfp_mask, nodemask_t *nodemask, const char *fmt, ...)
+void warn_alloc(gfp_t gfp_mask, const nodemask_t *nodemask, const char *fmt, ...)
 {
 	struct va_format vaf;
 	va_list args;
