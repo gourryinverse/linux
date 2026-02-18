2bda23b388c95318ab570fc088fbddb7e532bd93
Gregory Price <gourry@gourry.net>
2026-02-18 13:04:12 -0800

    mm/khugepaged: skip private node folios when trying to collapse.
    A collapse operation allocates a new large folio and migrates the
    smaller folios into it.  This is an issue for private nodes:
    
      1. The private node service may not support migration
      2. Collapse may promotes pages from the private node to a local node,
         which may result in an LRU inversion that defeats memory tiering.
    
    Handle this just like zone_device for now.
    
    It may be possible to support this later for some private node services
    that report explicit support for collapse (and migration).
    
    Signed-off-by: Gregory Price <gourry@gourry.net>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -21,6 +21,7 @@
 #include <linux/shmem_fs.h>
 #include <linux/dax.h>
 #include <linux/ksm.h>
+#include <linux/node_private.h>
 
 #include <asm/tlb.h>
 #include <asm/pgalloc.h>
@@ -585,7 +586,7 @@ static int __collapse_huge_page_isolate(struct vm_area_struct *vma,
 			goto out;
 		}
 		page = vm_normal_page(vma, address, pteval);
-		if (unlikely(!page) || unlikely(is_zone_device_page(page))) {
+		if (unlikely(!page) || unlikely(page_is_private_managed(page))) {
 			result = SCAN_PAGE_NULL;
 			goto out;
 		}
@@ -1333,7 +1334,7 @@ static int hpage_collapse_scan_pmd(struct mm_struct *mm,
 			writable = true;
 
 		page = vm_normal_page(vma, _address, pteval);
-		if (unlikely(!page) || unlikely(is_zone_device_page(page))) {
+		if (unlikely(!page) || unlikely(page_is_private_managed(page))) {
 			result = SCAN_PAGE_NULL;
 			goto out_unmap;
 		}
@@ -1569,7 +1570,7 @@ int collapse_pte_mapped_thp(struct mm_struct *mm, unsigned long addr,
 		}
 
 		page = vm_normal_page(vma, addr, ptent);
-		if (WARN_ON_ONCE(page && is_zone_device_page(page)))
+		if (WARN_ON_ONCE(page && page_is_private_managed(page)))
 			page = NULL;
 		/*
 		 * Note that uprobe, debugger, or MAP_PRIVATE may change the
