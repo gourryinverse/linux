d8766f24e6282cc344a85f06d2c1b147125f8643
Gregory Price <gourry@gourry.net>
2026-03-05 22:50:25 -0800

    mm/oom: PGMAP_OPS_OOM_ELIGIBLE - managed device node OOM participation
    Private nodes are only OOM-eligible if they support both reclaim and
    demotion, ensuring the OOM killer can actually free memory.
    
    Define PGMAP_OPS_OOM_ELIGIBLE as the composite of PGMAP_OPS_RECLAIM
    and PGMAP_OPS_DEMOTION.
    
    Move OOM eligibility checks from cpuset into oom_kill.c with
    node_oom_eligible() and zone_oom_eligible() helpers that check
    node_device_flags().
    
    Signed-off-by: Gregory Price <gourry@gourry.net>

--- include/linux/memremap.h
+++ include/linux/memremap.h
@@ -152,6 +152,7 @@ struct dev_pagemap_ops {
 #define PGMAP_OPS_DEMOTION	(1 << 3)
 #define PGMAP_OPS_PROTECT_WRITE	(1 << 4)
 #define PGMAP_OPS_RECLAIM	(1 << 5)
+#define PGMAP_OPS_OOM_ELIGIBLE	(PGMAP_OPS_RECLAIM | PGMAP_OPS_DEMOTION)
 
 /**
  * struct dev_pagemap - metadata for ZONE_DEVICE mappings
