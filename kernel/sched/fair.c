1c34692d2cfa48468b1a01249298e8e32ee8b2b4
Gregory Price <gourry@gourry.net>
2026-07-23 12:23:02 -0700

    mm: add N_MEMORY_NUMA_BALANCING to mark numa-balancing-eligible nodes
    A node only participates in NUMA Balancing if this node state is set.
    
    No behavioral change: all N_MEMORY nodes set N_MEMORY_NUMA_BALANCING.
    
    Signed-off-by: Gregory Price <gourry@gourry.net>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2680,10 +2680,8 @@ bool should_numa_migrate_memory(struct task_struct *p, struct folio *folio,
 	int dst_nid = cpu_to_node(dst_cpu);
 	int last_cpupid, this_cpupid;
 
-	/*
-	 * Cannot migrate to memoryless nodes.
-	 */
-	if (!node_state(dst_nid, N_MEMORY))
+	/* Cannot migrate to a node that does not support NUMA balancing. */
+	if (!node_state(dst_nid, N_MEMORY_NUMA_BALANCING))
 		return false;
 
 	/*
