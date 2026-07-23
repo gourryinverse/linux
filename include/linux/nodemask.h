97e6874e3ade46d77884680b3b99357e0074295e
Gregory Price <gourry@gourry.net>
2026-07-23 12:25:44 -0700

    mm: add N_MEMORY_DAMON node state for DAMON eligible nodes
    A node only participates in DAMON management if the N_MEMORY_DAMON
    node state is set for whichever folios it monitors.
    
    No behavioral change: all N_MEMORY nodes set N_MEMORY_DAMON.
    
    Signed-off-by: Gregory Price <gourry@gourry.net>

--- include/linux/nodemask.h
+++ include/linux/nodemask.h
@@ -396,6 +396,7 @@ enum node_states {
 	N_MEMORY_DEMOTION,	/* tiering may demote to the node */
 	N_MEMORY_NUMA_BALANCING,	/* NUMA balancing may migrate to the node */
 	N_MEMORY_LTPIN,		/* long-term GUP pins are allowed on the node */
+	N_MEMORY_DAMON,		/* DAMON may operate on the node */
 	N_CPU,		/* The node has one or more cpus */
 	N_GENERIC_INITIATOR,	/* The node has one or more Generic Initiators */
 	NR_NODE_STATES
@@ -528,10 +529,12 @@ static inline void node_set_memory_state(int nid, bool high, bool normal)
 	node_set_state(nid, N_MEMORY_DEMOTION);
 	node_set_state(nid, N_MEMORY_NUMA_BALANCING);
 	node_set_state(nid, N_MEMORY_LTPIN);
+	node_set_state(nid, N_MEMORY_DAMON);
 }
 
 static __always_inline void node_clear_memory_state(int nid)
 {
+	node_clear_state(nid, N_MEMORY_DAMON);
 	node_clear_state(nid, N_MEMORY_LTPIN);
 	node_clear_state(nid, N_MEMORY_NUMA_BALANCING);
 	node_clear_state(nid, N_MEMORY_DEMOTION);
