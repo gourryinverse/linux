9f8f0aaafbecd05efcee41150c8f51dee4cff858
Gregory Price <gourry@gourry.net>
2026-07-26 14:12:03 -0700

    mm: add N_MEMORY_USER_NUMA node state to mark userland numactl support
    A node only allows userland numa controls if the N_MEMORY_USER_NUMA bit
    is set.  This includes both migration and mempolicy controls.
    
    The cpuset/mempolicy interaction is left mostly unmodified here, except
    for do_migrate_pages - which now filters on N_MEMORY_USER_NUMA.  This
    will prevent a cpuset.mems change from migrating memory to/from a node
    without N_MEMORY_USER_NUMA.
    
    No behavioral change: all N_MEMORY nodes set N_MEMORY_USER_NUMA.
    
    Signed-off-by: Gregory Price <gourry@gourry.net>

--- include/linux/nodemask.h
+++ include/linux/nodemask.h
@@ -402,6 +402,7 @@ enum node_states {
 	N_MEMORY_PUBLIC,	/* general-purpose pool (fallback zonelist) */
 	N_MEMORY_RECLAIM,	/* mm reclaim may operate on the node */
 	N_MEMORY_DEMOTION,	/* tiering may demote to the node */
+	N_MEMORY_USER_NUMA,	/* userspace NUMA placement may target the node */
 	N_CPU,		/* The node has one or more cpus */
 	N_GENERIC_INITIATOR,	/* The node has one or more Generic Initiators */
 	NR_NODE_STATES
@@ -539,10 +540,12 @@ static inline void node_set_memory_state(int nid, bool high, bool normal)
 	node_set_state(nid, N_MEMORY_PUBLIC);
 	node_set_state(nid, N_MEMORY_RECLAIM);
 	node_set_state(nid, N_MEMORY_DEMOTION);
+	node_set_state(nid, N_MEMORY_USER_NUMA);
 }
 
 static __always_inline void node_clear_memory_state(int nid)
 {
+	node_clear_state(nid, N_MEMORY_USER_NUMA);
 	node_clear_state(nid, N_MEMORY_DEMOTION);
 	node_clear_state(nid, N_MEMORY_RECLAIM);
 	node_clear_state(nid, N_MEMORY_PUBLIC);
